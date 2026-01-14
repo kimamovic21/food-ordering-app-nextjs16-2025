63cdab803de87b2c934b9bea3374f378414acace
Kerim Imamović <imamovic.kerim@gmail.com>
2026-01-14 17:30:41 +0100

    Folder update for cn function
    Relocated utils.ts from lib to libs directory and updated all UI components to import the cn function from the new location. This improves project structure and maintains consistency in utility imports.

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -12,8 +12,7 @@ import {
   type FieldPath,
   type FieldValues,
 } from 'react-hook-form';
-
-import { cn } from '@/lib/utils';
+import { cn } from '../../libs/utils';
 import { Label } from '@/components/ui/label';
 
 const Form = FormProvider;
--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ChevronLeftIcon, ChevronRightIcon, MoreHorizontalIcon } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
+import { cn } from '../../libs/utils';
 import { buttonVariants, type Button } from '@/components/ui/button';
 
 function Pagination({ className, ...props }: React.ComponentProps<'nav'>) {
